f32be56b6599bba75e5a9e0cfdf6f110ff397205
jominkook <whalsrnraos@naver.com>
2024-11-25 23:04:53 +0900

    Solved-2

---
+++ Solved_20125_1.py
@@ -0,0 +1,47 @@
+import sys
+
+N = int(sys.stdin.readline())
+cookie = [[] *N for _ in range(N)]
+
+for i in range(N):
+    cookie[i] = list(input())
+    
+
+def heart():
+    for i in range(N):
+        for j in range(N):
+            if cookie[i][j] == '*':
+                return i+2,j+1
+            
+def left(x,y):
+    cnt = 0
+    while y >= 0 and cookie[x][y] == '*':
+        cnt += 1
+        y -= 1
+    return cnt
+
+def right(x,y):
+    cnt = 0
+    while y < N and cookie[x][y] == '*':
+        cnt += 1
+        y += 1
+    return cnt
+
+def lower(x,y):
+    cnt = 0
+    while x < N and cookie[x][y] == '*':
+        cnt += 1
+        x += 1
+    return cnt
+
+heartX,heartY = heart()
+
+x = heartX -1
+y = heartY -1
+
+waist = lower(x+1,y)
+
+body = [left(x, y-1), right(x, y+1), lower(x+1, y), lower(x+waist+1, y-1), lower(x+waist+1, y+1)]
+
+print(heartX,heartY)
+print(*body)

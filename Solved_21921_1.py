ff229653cd4e9df13bed3d38b940efe3adf2e944
jominkook <whalsrnraos@naver.com>
2024-12-10 22:29:52 +0900

    Solved-17

---
+++ Solved_21921_1.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N,X = map(int,input().split())
+
+visit = list(map(int,input().split()))
+
+if sum(visit[:N]) == 0:
+    print("SAD")
+    sys.exit()
+
+for i in range(N):
+    visit[i] = sum(visit[i:i+X])
+    
+  
+    
+max_visit = max(visit)
+max_count = visit.count(max_visit)
+
+
+
+print(max_visit)
+print(max_count)
+
+
+

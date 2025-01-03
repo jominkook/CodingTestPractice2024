946e5962681b796fe416c5d7ec8de9bc43fb2e57
jominkook <whalsrnraos@naver.com>
2025-01-03 18:06:49 +0900

    Solved-42 restart

--- Solved_2607_1.py
+++ Solved_2607_1.py
@@ -1,26 +1,23 @@
+#2607 비슷한 단어
 import sys
 sys.stdin.readline
 
 N = int(input())
-word = str(input())
-target = []
-for i in range(N-1):
-    target.append(str(input()))
-    
-answer = 0
+target = list(input())
+
+result = 0  
 
 for i in range(N-1):
-    if len(word) != len(set(target[i])):
-        continue
-    
-    if len(word) == len(set(target[i])):
-        for j in range(len(word)):
-            if word[j] in target[i]:
-                continue
-            else:
-                break
+    word = list(input())
+    temp = target.copy()
+    cnt = 0
+    for j in range(len(word)):
+        if word[j] in temp:
+            temp.remove(word[j])
         else:
-            answer += 1
-            
-print(answer)
+            cnt += 1
+    if cnt == 1 or cnt == 0 and abs(len(word) - len(target)) == 1:
+        result += 1
+print(result)
+ 
 

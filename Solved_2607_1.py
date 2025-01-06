18c93ace48f91a941edd95c5e3c3d68169614fa3
jominkook <whalsrnraos@naver.com>
2025-01-06 13:47:07 +0900

    Solved-42-3

--- Solved_2607_1.py
+++ Solved_2607_1.py
@@ -1,23 +1,22 @@
-#2607 비슷한 단어
-import sys
-sys.stdin.readline
+#백준 2607 비슷한단어
 
 N = int(input())
+
 target = list(input())
 
-result = 0  
+result = 0
+diff = 0
 
 for i in range(N-1):
     word = list(input())
-    temp = target.copy()
-    cnt = 0
-    for j in range(len(word)):
-        if word[j] in temp:
-            temp.remove(word[j])
+    diff = 0
+    for j in target:
+        if j in word:
+            word.remove(j)
         else:
-            cnt += 1
-    if cnt == 1 or cnt == 0 and abs(len(word) - len(target)) == 1:
+            diff += 1
+            
+    if diff < 2 and len(word) <2:
         result += 1
+        
 print(result)
- 
-

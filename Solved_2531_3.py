ca0fad31db75ebc1cd12112c5da0392cd7a71528
jominkook <whalsrnraos@naver.com>
2025-01-07 12:44:10 +0900

    Solved-44

---
+++ Solved_2531_3.py
@@ -0,0 +1,32 @@
+n,d,k,c = map(int,input().split())
+
+sushi = []
+
+for i in range(n):
+    sushi.append(int(input()))
+    
+sushi += sushi[:k-1]
+kind = [0] * (d+1)
+
+for i in range(k):
+    kind[sushi[i]] += 1
+cnt = len(set(sushi[:k]))
+
+result = cnt
+
+for i in range(n):
+    kind[sushi[i]] -= 1
+    if kind[sushi[i]] == 0:
+        cnt -= 1
+    kind[sushi[(i+k)%n]] += 1
+    if kind[sushi[(i+k)%n]] == 1:
+        cnt += 1
+    result = max(result,cnt + (c not in sushi[i+1:i+k]))
+    
+print(result)
+
+
+
+
+
+

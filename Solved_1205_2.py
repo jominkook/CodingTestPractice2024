6d1febb59e3c86c2ca773b8b639df7d06f1ce96f
jominkook <whaksenaos@naver.com>
2025-03-06 01:44:43 +0900

    Solved-72회차

---
+++ Solved_1205_2.py
@@ -0,0 +1,22 @@
+#백준 1205번 등수 구하기
+# 2번째 풀이
+N, NEW_SCORE, P = map(int, input().split())
+scores = []
+if N == 0 and NEW_SCORE == 0 and P == 0:
+    print(1)
+else:
+    scores = list(map(int, input().split()))
+    scores.append(NEW_SCORE)
+    scores.sort(reverse=True)
+    if N == P and scores[-1] >= NEW_SCORE:
+        print(-1)
+    else:
+        print(scores.index(NEW_SCORE) + 1)
+        
+        
+
+                
+          
+                
+                
+    
